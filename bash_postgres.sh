726208fcda7ee1ab2b54790766d1fc9650c48856
Mikolaj Luczyszyn <mikolaj.luczyszyn@gfieast.com>
2020-11-25 13:28:45 +0100

    bash.postgres

---
+++ bash_postgres.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+
+psql -U postgres -c "create database test;"
+
+psql -U postgres -d test -c "create table aplication (name varchar(100) NOT NULL); insert INTO aplication values ('JOHN');" 
